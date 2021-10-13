df202fc8b0e9b5741a176f6519ee81cd66650d35
Bolutife Ogunsola <bostikforever@gmail.com>
2021-10-13 16:37:39 +0100

    Use node based structure to store markers
    Makes it possible to share marker objects across Cursors, thus
    avoiding copying. Cursor creation is now O(1).

--- src/cursor.py
+++ src/cursor.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
-import copy
 
-from typing import List, Optional
+from typing import Iterable, Optional
 
 
 class _Marker:
-    def __init__(self, offset: int, character: str):
+    def __init__(self, offset: int, character: str, prev: Optional[_Marker] = None):
+        # ensure markers is sorted
+        assert prev is None or offset > prev.offset
         assert len(character) == 1
-        self.offset = offset
+        self.offset: int = offset
         self.character = character
+        self.prev = prev
 
 
 class Cursor:
@@ -64,18 +66,12 @@ class Cursor:
     ..
     """
 
-    def __init__(
-        self, offset: Optional[int] = None, markers: Optional[List[_Marker]] = None
-    ):
+    def __init__(self, offset: Optional[int] = None, markers: Optional[_Marker] = None):
         if offset is None:
             offset = 0
-        # assumes markers is sorted
-        if markers:
-            last_marker = markers[-1]
-            assert last_marker.offset < offset
+        assert markers is None or markers.offset < offset
         self._offset = offset
-        # can use a node-based structure instead to save memory
-        self._markers = copy.copy(markers) if markers is not None else []
+        self._markers = markers
 
     def print(self, *strings) -> Cursor:
         accum_len = 0
@@ -85,15 +81,22 @@ class Cursor:
         return Cursor(self._offset + accum_len, self._markers)
 
     def add_marker(self, character) -> Cursor:
-        assert not self._markers or self._markers[-1].offset < self._offset
+        assert not self._markers or self._markers.offset < self._offset
         print(character, end="")
-        marker = _Marker(self._offset, character)
-        return Cursor(marker.offset + 1, self._markers + [marker])
+        new_markers = _Marker(self._offset, character, self._markers)
+        return Cursor(new_markers.offset + 1, new_markers)
+
+    def _accumulate_markers_and_offset(self) -> Iterable[str]:
+        reversed_segments = []
+        marker = self._markers
+        position = self._offset
+        while marker is not None:
+            reversed_segments.append(" " * (position - marker.offset - 1))
+            reversed_segments.append(marker.character)
+            position = marker.offset
+            marker = marker.prev
+        reversed_segments.append(" " * position)
+        return reversed(reversed_segments)
 
     def advance_line(self):
-        position = 0
-        print()
-        for marker in self._markers:
-            print(" " * (marker.offset - position), marker.character, sep="", end="")
-            position = marker.offset + 1
-        print(" " * (self._offset - position), end="")
+        print("\n", *self._accumulate_markers_and_offset(), sep="", end="")
